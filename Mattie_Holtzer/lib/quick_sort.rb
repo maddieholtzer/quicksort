679f84a146a92e46e460917b101029c14f2847e5
Mattie <mattieholtzer@gmail.com>
2018-05-03 22:44:07 -0700

    working out of order

--- Mattie_Holtzer/lib/quick_sort.rb
+++ Mattie_Holtzer/lib/quick_sort.rb
@@ -4,10 +4,20 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length <= 1
+    pivot = array[0]
+    arr2 = []
+    arr3 = []
+    array[1..array.length].each do |val|
+      arr2.push(val) if val<=pivot
+      arr3.push(val) if val>pivot
+    end
+    QuickSort.sort1(arr2) + [pivot] + QuickSort.sort1(arr3)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    QuickSort.sort1(array)
   end
 
   def self.partition(array, start, length, &prc)
